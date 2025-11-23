e1aaa2a09e29a6c78d29355aafb7c896e36aee54
Metehan Kızılcık <79705009+mo-tunn@users.noreply.github.com>
2025-11-23 14:50:12 +0300

    TestBird ve SnakeGame Dokümantasyon

---
+++ Mikro Projeler/SnakeGame ve Dokümanlar/6-Snake_Game/Backup/Snake_Game/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+
+#Region "Snake Staff"
+    Dim snake(1000) As PictureBox
+    Dim lenght_of_Snake As Integer = -1
+    Dim left_right_mover As Integer = 0
+    Dim up_down_mover As Integer = 0
+    Dim r As New Random
+    Dim Mouse = New PictureBox
+
+
+    Private Sub create_head()
+        lenght_of_Snake += 1
+        snake(lenght_of_Snake) = New PictureBox
+        With snake(lenght_of_Snake)
+            .Height = 10
+            .Width = 10
+            .BackColor = Color.White
+            .Top = (pb_Field.Top + pb_Field.Bottom) / 2
+            .Left = (pb_Field.Left + pb_Field.Right) / 2
+        End With
+        Me.Controls.Add(snake(lenght_of_Snake))
+        snake(lenght_of_Snake).BringToFront()
+        lenght_Snake()
+    End Sub
+    Private Sub lenght_Snake()
+        lenght_of_Snake += 1
+        snake(lenght_of_Snake) = New PictureBox
+        With snake(lenght_of_Snake)
+            .Height = 10
+            .Width = 10
+            .BackColor = Color.White
+            .Top = snake(lenght_of_Snake - 1).Top
+            .Left = snake(lenght_of_Snake - 1).Left
+        End With
+        Me.Controls.Add(snake(lenght_of_Snake - 1))
+        snake(lenght_of_Snake).BringToFront()
+    End Sub
+    Private Sub Form1_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Me.KeyPress
+        tm_SnakeMover.Start()
+        Select Case e.KeyChar
+            Case "a"
+                left_right_mover = -10
+                up_down_mover = 0
+            Case "d"
+                left_right_mover = 10
+                up_down_mover = 0
+            Case "w"
+                left_right_mover = 0
+                up_down_mover = -10
+            Case "s"
+                left_right_mover = 0
+                up_down_mover = 10
+        End Select
+
+    End Sub
+    Private Sub tm_SnakeMover_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tm_SnakeMover.Tick
+        For i = lenght_of_Snake To 1 Step -1
+            snake(i).Top = snake(i - 1).Top
+            snake(i).Left = snake(i - 1).Left
+        Next
+
+        snake(0).Top += up_down_mover
+        snake(0).Left += left_right_mover
+        collide_With_walls()
+        collide_with_mouse()
+        collide_with_itself()
+    End Sub
+#End Region
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        create_head()
+        Create_mouse()
+        'tm_SnakeMover.Start()
+    End Sub
+#Region "Collation"
+    Private Sub collide_With_walls()
+        If snake(0).left < pb_Field.left Then
+            tm_SnakeMover.Stop()
+            MsgBox("You Lost")
+        End If
+        If snake(0).Right > pb_Field.Right Then
+            tm_SnakeMover.Stop()
+            MsgBox("You Lost")
+        End If
+        If snake(0).Top < pb_Field.Top Then
+            tm_SnakeMover.Stop()
+            MsgBox("You Lost")
+        End If
+        If snake(0).Bottom > pb_Field.Bottom Then
+            tm_SnakeMover.Stop()
+            MsgBox("You Lost")
+        End If
+    End Sub
+    Private Sub collide_with_mouse()
+        If snake(0).Bounds.IntersectsWith(Mouse.bounds) Then
+            lenght_Snake()
+            Mouse.Left = r.Next(pb_Field.Left, pb_Field.Right - 10)
+            Mouse.Top = r.Next(pb_Field.Top, pb_Field.Bottom - 10)
+        End If
+    End Sub
+    Private Sub collide_with_itself()
+        For i = 1 To lenght_of_Snake - 1
+            If snake(0).Bounds.IntersectsWith(snake(i).Bounds) Then
+                tm_SnakeMover.Stop()
+                MsgBox("You Lost Lose")
+
+            End If
+        Next
+    End Sub
+#End Region
+#Region "Mouse Staff"
+    Private Sub Create_mouse()
+        With Mouse
+            .Width = 10
+            .Height = 10
+            .BackColor = Color.Red
+            .Left = r.Next(pb_Field.Left, pb_Field.Right - 10)
+            .Top = r.Next(pb_Field.Top, pb_Field.Bottom - 10)
+        End With
+        Me.Controls.Add(Mouse)
+        Mouse.BringToFront()
+    End Sub
+#End Region
+
+    Private Sub pb_Field_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pb_Field.Click
+
+    End Sub
+End Class
---
+++ Mikro Projeler/SnakeGame ve Dokümanlar/6-Snake_Game/Snake_Game/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pb_Field = New System.Windows.Forms.PictureBox()
+        Me.tm_SnakeMover = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pb_Field, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb_Field
+        '
+        Me.pb_Field.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.pb_Field.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pb_Field.Location = New System.Drawing.Point(12, 16)
+        Me.pb_Field.Name = "pb_Field"
+        Me.pb_Field.Size = New System.Drawing.Size(620, 454)
+        Me.pb_Field.TabIndex = 0
+        Me.pb_Field.TabStop = False
+        Me.pb_Field.Visible = False
+        '
+        'tm_SnakeMover
+        '
+        Me.tm_SnakeMover.Enabled = True
+        Me.tm_SnakeMover.Interval = 50
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(644, 482)
+        Me.Controls.Add(Me.pb_Field)
+        Me.Name = "Form1"
+        Me.Text = "Snake Game"
+        CType(Me.pb_Field, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pb_Field As System.Windows.Forms.PictureBox
+    Friend WithEvents tm_SnakeMover As System.Windows.Forms.Timer
+
+End Class
